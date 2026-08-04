5f165cbcc1c2dfac44386bdf36e9f87396beefd3
yyjkimm0 <yyjkimm0@gmail.com>
2026-08-04 21:06:03 +0900

    style: align fourth navigation with fifth lab

--- src/labs/fourth/App.vue
+++ src/labs/fourth/App.vue
@@ -6,8 +6,10 @@ import './assets/lab.css'
 <template>
   <section class="fourth-lab">
     <nav class="app-navigation" aria-label="네 번째 과제 메뉴">
-      <RouterLink :to="{ name: 'fourth-weather-home' }">날씨 대시보드</RouterLink>
-      <RouterLink :to="{ name: 'fourth-weather-about' }">서비스 소개</RouterLink>
+      <div class="app-navigation__links app-navigation__links--center">
+        <RouterLink :to="{ name: 'fourth-weather-home' }">날씨 대시보드</RouterLink>
+        <RouterLink :to="{ name: 'fourth-weather-about' }">서비스 소개</RouterLink>
+      </div>
     </nav>
 
     <RouterView />
