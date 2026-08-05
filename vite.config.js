30897ac46543d098575687471e29ab25773131b7
yyjkimm0 <yyjkimm0@gmail.com>
2026-08-05 14:05:12 +0900

    fix: use root asset base for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/skala-vue/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,4 +16,4 @@ export default defineConfig(({ command }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}))
+})
